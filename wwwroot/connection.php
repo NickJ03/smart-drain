cc8a5143ccfee40f68725998143c6d0e24b3bfd6
nickj <n.jans@student.tue.nl>
2023-10-10 12:02:48 +0200

    log error popup made. it does not work yet properly

--- wwwroot/connection.php
+++ wwwroot/connection.php
@@ -7,6 +7,10 @@ This software is made available under the terms of the GNU General Public Licens
 // path to log file
 $logs_file_path = __DIR__ . "/logs/log.txt";
 
+// force errors to be displayed in the log
+ini_set('display_errors', 0);
+error_reporting(E_ALL);
+
 // clear previous log and start new log file
 file_put_contents($logs_file_path, "");
 $message = "Log created " . date("Y-m-d H:i:s") . "\n" . str_repeat("-", 100) . "\n";
@@ -16,7 +20,7 @@ file_put_contents($logs_file_path, $message, FILE_APPEND);
 $server = "localhost";
 $user = "245";
 $pass = "245";
-$db = "smart_drain";
+$db = "dumb_drain";
 
 // make new connection with MySQL database
 $conn = new mysqli($server, $user, $pass, $db);
@@ -25,11 +29,27 @@ $conn = new mysqli($server, $user, $pass, $db);
 // if unsuccessful, exit the script and display the error code
 if ($conn->connect_error) {
     $message = date("Y-m-d H:i:s") . " - Connection failed: " . $conn->connect_error . "\n";
+    file_put_contents($logs_file_path, $message, FILE_APPEND);
+
+    // open error log window (this uses JavaScript)
+?>
+
+<script>
+    let logFilePath = "<?php echo $logs_file_path; ?>";
+    let popUpName = "Error log";
+
+    window.open(logFilePath, popUpName, "_blank");
+</script>
+
+<?php
+
+    // exit script
     die();
 } else {
     $message = date("Y-m-d H:i:s") . " - Connection successful\n";
+    file_put_contents($logs_file_path, $message, FILE_APPEND);
 }
-file_put_contents($logs_file_path, $message, FILE_APPEND);
+
 
 // check request type POST and connect variables from microcontroller
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
