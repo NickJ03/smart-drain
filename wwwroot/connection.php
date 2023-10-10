661c2cf31800f6812a1a0ca6daade2670323dbc1
nickj <n.jans@student.tue.nl>
2023-10-10 10:31:01 +0200

    added POST connection and SQL insertion. Note: not properly tested yet as the connection cannot be realised yet

--- wwwroot/connection.php
+++ wwwroot/connection.php
@@ -21,12 +21,31 @@ if ($conn->connect_error) {
     echo "Connection successful";
 }
 
+// check request type POST and connect variables from microcontroller
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $volume = $_POST["//"];
+
+    // insert data into connected MySQL database
+    // generate SQL query
+    $insert_query = "INSERT INTO water_usage (volume) VALUES ($volume)";
+
+    // perform and check query
+    // note: if no error occurs, $conn->query($insert_query) returns TRUE. the condition == TRUE is implicit here
+    // if unsuccessful, display the error code and the generated query
+    if ($conn->query($insert_query)) {
+        echo "Insertion successful";
+    } else {
+        echo "Insertion unsuccessful" . "<br>" . "Error: " . $insert_query . "<br>" . $conn->error;
+    }
+}
+
+// close the database connection
+$conn->close();
+
 // the data from the microcontroller should be sent via POST request (controller side)
 // the data needs to be placed in variable with $name = $_POST['name_on_controller']
 // the data needs to be inserted into the database. before doing this, the database tables must be created
 // the query execution must be checked
 // the connection must be closed at the end
 
-
-
-
+?>
